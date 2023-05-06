a0bef5f3a11ae82807b7074d92d782a0f9d70834
shinchan <32698751+shinchankosen@users.noreply.github.com>
2023-05-06 17:58:34 +0900

    Create chall.py

---
+++ mis-int-generator/chall.py
@@ -0,0 +1,77 @@
+import random
+
+k = 36
+maxlength = 16
+
+
+def f(x, cnt):
+    cnt += 1
+    r = 2**k
+    if x == 0 or x == r:
+        return -x, cnt
+    if x * x % r != 0:
+        return -x, cnt
+    else:
+        return -x * (x - r) // r, cnt
+
+
+def g(x):
+    ret = x * 2 + x // 3 * 10 - x // 5 * 10 + x // 7 * 10
+    ret = ret - ret % 2 + 1
+    return ret, x // 100 % 100
+
+
+def digit(x):
+    cnt = 0
+    while x > 0:
+        cnt += 1
+        x //= 10
+    return cnt
+
+
+def pad(x, cnt):
+    minus = False
+    if x < 0:
+        minus = True
+        x, cnt = g(-x)
+    sub = maxlength - digit(x)
+    ret = x
+    for i in range(sub - digit(cnt)):
+        ret *= 10
+        if minus:
+            ret += pow(x % 10, x % 10 * i, 10)
+        else:
+            ret += pow(i % 10 - i % 2, i % 10 - i % 2 + 1, 10)
+    ret += cnt * 10 ** (maxlength - digit(cnt))
+    return ret
+
+
+def int_generator(x):
+    ret = -x
+    x_, cnt = f(x, 0)
+    while x_ > 0:
+        ret = x_
+        x_, cnt = f(x_, cnt)
+    return pad(ret, cnt)
+
+X = 1008844668800884
+for i in range(100000000):
+    if int_generator(i) == X:
+        print(i)
+        break
+
+X = 2264663430088446
+for i in range(7000000, 100000000):
+    if i % 10000 == 0:
+        print(i)
+        
+    if int_generator(i) == X:
+        print(i)
+        break
+
+X = 6772814078400884
+for i in range(1 << 35, 0, -1):
+    if int_generator(i) == X:
+        print(i)
+        break
+
